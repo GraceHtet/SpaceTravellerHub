2229c9dece594a32bd74b5cd84109feff9111559
Thiri <thirihtet246@gmail.com>
2023-03-07 07:38:55 +0630

    Setup Rockets
    Fetch rockets data with axios
    Displayed rockets

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,4 +1,37 @@
-const Rockets = () => (
-  <div>Rockets</div>
-);
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { selectRockets, fetchRockets, getStatus } from '../redux/rockets/rocketsSlice';
+import styles from '../styles/Rockets.module.css';
+
+const Rockets = () => {
+  const dispatch = useDispatch();
+  const rockets = useSelector(selectRockets);
+  const status = useSelector(getStatus);
+
+  useEffect(() => {
+    if (status === false) dispatch(fetchRockets());
+  }, [dispatch, status]);
+
+  let content;
+  if (status === true) {
+    content = rockets.map((each) => (
+      <article key={each.id} className={styles.article}>
+        <img src={each.flickr_images} className={styles.img} alt="rocket" />
+        <div>
+          <h2 className={styles.title}>{each.name}</h2>
+          <p>
+            <span />
+            {each.description}
+          </p>
+          <button type="button" className={styles.btn}>
+            Reserve Rocket
+          </button>
+        </div>
+      </article>
+    ));
+  }
+
+  console.log(`${content}...leo`);
+  return <>{content}</>;
+};
 export default Rockets;
--- src/redux/rockets/rocketsSlice.js
+++ src/redux/rockets/rocketsSlice.js
@@ -4,7 +4,8 @@ import axios from 'axios';
 const url = 'https://api.spacexdata.com/v4/rockets';
 
 const initialState = {
-  rockets: [],
+  rocketLists: [],
+  isLoading: false,
 };
 
 export const fetchRockets = createAsyncThunk('rockets/fetchRockets', async () => {
@@ -16,6 +17,17 @@ const rocketsSlice = createSlice({
   name: 'rockets',
   initialState,
   reducers: {},
+  extraReducers(builder) {
+    builder
+      .addCase(fetchRockets.pending, (state) => ({ ...state, isLoading: true }))
+      .addCase(fetchRockets.fulfilled, (state, action) => {
+        console.log(action.payload);
+        return { rocketLists: action.payload, isLoading: true };
+      });
+  },
 });
+
+export const selectRockets = (state) => state.rockets.rocketLists;
+export const getStatus = (state) => state.rockets.isLoading;
 
 export default rocketsSlice.reducer;
