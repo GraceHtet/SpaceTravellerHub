653822da63b963f87388077a4db36c5d5fa19106
Thiri <thirihtet246@gmail.com>
2023-03-08 21:26:23 +0630

    Add Button action test

--- src/__test__/rocket.test.js
+++ src/__test__/rocket.test.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
-import TestRenderer from 'react-test-renderer';
-// import { render, screen, fireEvent } from '@testing-library/react';
-import store from '../redux/store';
+import '@testing-library/jest-dom/extend-expect';
+import TestRenderer, { act } from 'react-test-renderer';
 import Rockets from '../components/Rockets';
+import store from '../redux/store';
 
 describe('Rockets', () => {
   test('Render Rockets Component', () => {
@@ -13,5 +13,63 @@ describe('Rockets', () => {
       </Provider>,
     ).toJSON();
     expect(tree).toMatchSnapshot();
+  });
+
+  test('Reserve Rocket Buttion', async () => {
+    render(
+      <Provider store={store}>
+        <Rockets />
+      </Provider>,
+    );
+    const reserveBtn = await screen.findAllByText('Reserve Rocket');
+    expect(reserveBtn).toHaveLength(4);
+  });
+
+  test('Should Appear Reserved text when click on Reserved Button ', async () => {
+    render(
+      <Provider store={store}>
+        <Rockets />
+      </Provider>,
+    );
+
+    act(() => {
+      const reserveBtn = screen.getAllByText('Reserve Rocket');
+      reserveBtn[0].click();
+    });
+
+    const reserveText = await screen.getByText('Reserved');
+    expect(reserveText).toBeInTheDocument();
+  });
+
+  test('Cancel Reservation Buttion', async () => {
+    render(
+      <Provider store={store}>
+        <Rockets />
+      </Provider>,
+    );
+
+    const cancelBtn = await screen.findAllByText('Cancel Reservation');
+    expect(cancelBtn).toHaveLength(1);
+  });
+
+  test('Should Disappear one Reserved text when click on Cancel Reservation', async () => {
+    render(
+      <Provider store={store}>
+        <Rockets />
+      </Provider>,
+    );
+
+    act(() => {
+      const reserveBtn = screen.getAllByText('Reserve Rocket');
+      reserveBtn[1].click();
+    });
+
+    act(() => {
+      const cancelBtn = screen.getAllByText('Cancel Reservation');
+      cancelBtn[0].click();
+    });
+
+    const reserveText = await screen.findAllByText('Reserved');
+    expect(reserveText).toHaveLength(1);
   });
 });
--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,6 +1,5 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { useEffect } from 'react';
-// eslint-disable-next-line object-curly-newline
 import { fetchRockets, reserveRocket } from '../redux/rockets/rocketsSlice';
 import styles from '../styles/Rockets.module.css';
 
@@ -13,26 +12,35 @@ const Rockets = () => {
     if (status === false) dispatch(fetchRockets());
   }, [dispatch, status]);
 
-  const rockets = rocketLists.map((each) => (
-    <article key={each.id} className={styles.article}>
-      <img src={each.flickr_images} className={styles.img} alt="rocket" />
-      <div>
-        <h2 className={styles.title}>{each.name}</h2>
-        <p>
-          {each.reserved ? <span className={styles.span}>Reserved</span> : ''}
-          {each.description}
-        </p>
-        <button
-          type="button"
-          className={each.reserved ? styles.unreserve : styles.reserve}
-          onClick={() => dispatch(reserveRocket(each.id))}
-        >
-          {each.reserved ? 'Cancel Reservation' : 'Reserve Rocket'}
-        </button>
-      </div>
-    </article>
-  ));
+  const rockets = status
+    ? rocketLists.map((each) => (
+      <article key={each.id} className={styles.article}>
+        <img src={each.flickr_images} className={styles.img} alt="rocket" />
+        <div>
+          <h2 className={styles.title}>{each.name}</h2>
+          <p>
+            {each.reserved ? (
+              <span className={styles.span} data-testid={`span-${each.id}`}>
+                Reserved
+              </span>
+            ) : (
+              ''
+            )}
+            {each.description}
+          </p>
+          <button
+            type="button"
+            className={each.reserved ? styles.unreserve : styles.reserve}
+            data-testid={`btn-${each.id}`}
+            onClick={() => dispatch(reserveRocket(each.id))}
+          >
+            {each.reserved ? 'Cancel Reservation' : 'Reserve Rocket'}
+          </button>
+        </div>
+      </article>
+    ))
+    : '';
 
-  return <>{status && rockets}</>;
+  return <div>{rockets}</div>;
 };
 export default Rockets;
