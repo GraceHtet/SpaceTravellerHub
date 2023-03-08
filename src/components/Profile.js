3a8d5043e33a1c903cb3ecb29fee34d239c86465
shasherazi <hassanrandomz@gmail.com>
2023-03-08 13:15:45 +0500

    Render missions on profile page

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,13 +2,27 @@ import { useSelector } from 'react-redux';
 import styles from '../styles/Profile.module.css';
 
 const Profile = () => {
-  const { rocketLists } = useSelector((state) => state.rockets);
+  const { rockets } = useSelector((state) => state.rockets);
+  const { missions } = useSelector((state) => state.missions);
 
-  const reserveRockets = rocketLists.filter((rocket) => rocket.reserved && rocket);
+  const reserveRockets = rockets.filter((rocket) => rocket.reserved && rocket);
+  const joinedMissions = missions.filter((mission) => mission.joined);
 
   return (
     <div className={styles.container}>
-      <div />
+      <div className={styles.mission}>
+        <h2>My Missions</h2>
+        <table className={styles.table}>
+          <tbody className={styles.tbody}>
+            {joinedMissions.map((mission) => (
+              <tr key={mission.mission_id} className={styles.tr}>
+                <td>{mission.mission_name}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+
       <div className={styles.rocket}>
         <h2>My Rockets</h2>
         <table className={styles.table}>
